25dce665d1ee2be44a0a46d7b82f1fdbb253f8a9
Paulo Pamplona <pamplonapaulo@users.noreply.github.com>
2021-10-29 12:33:34 -0300

    issues with Vercel deploy

--- src/components/CartAction/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  margin-top: 2rem;
-  width: 95%;
-`
