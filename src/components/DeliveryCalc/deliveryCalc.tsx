2b903a02b7c155470780ba95dc74530779bb2303
Paulo Pamplona <pamplonapaulo@users.noreply.github.com>
2021-10-31 16:36:05 -0300

    melhor envio hard time

--- src/components/DeliveryCalc/deliveryCalc.tsx
+++ src/components/DeliveryCalc/deliveryCalc.tsx
@@ -6,22 +6,19 @@ import { Snack } from 'types/api'
 
 const DeliveryCalc = ({ pack }: { pack: Snack[] }) => {
   const [postcode, setPostcode] = useState('')
-  // const [quotation, setQuotation] = useState(0)
 
   useEffect(() => {
     if (postcode.length > 8) {
       const cepWithoutDash = postcode.replace('-', '')
-      console.log('pack')
-      console.log(pack)
       const myHeaders = new Headers()
       myHeaders.append('Accept', 'application/json')
       myHeaders.append('Content-Type', 'application/json')
-      myHeaders.append('Access-Control-Allow-Origin', '*')
-      myHeaders.append('User-Agent', 'Gorilla Pack (pamplonapaulo@gmail.com)')
       myHeaders.append(
         'Authorization',
         'Bearer {{eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImUzOTEwZGEzOTFjNmMwZmU2OWVjZmI0ODBhNjg0ODI5YTM1NzUyZmUzMDJjZTAzYTJmYmE0NTRjZmRjODNjNDU3YmZmOTE2YjI0MzE2N2RiIn0.eyJhdWQiOiI5NTYiLCJqdGkiOiJlMzkxMGRhMzkxYzZjMGZlNjllY2ZiNDgwYTY4NDgyOWEzNTc1MmZlMzAyY2UwM2EyZmJhNDU0Y2ZkYzgzYzQ1N2JmZjkxNmIyNDMxNjdkYiIsImlhdCI6MTYzNTU0MjYxMSwibmJmIjoxNjM1NTQyNjExLCJleHAiOjE2NjcwNzg2MTEsInN1YiI6ImNhYjUwYmZhLWM0YWUtNDIyYy1hYjlmLWQ1MGFhN2I3NGI2NiIsInNjb3BlcyI6WyJlY29tbWVyY2Utc2hpcHBpbmciXX0.k7xly_DQXZ9jMfOyUrzt_gPKw3SHPzIfp6uICo1Y2GXnZnG9Kl8hl9OK5zgLNuA9ZKNiTFAc_4br_qNbKLxitWVar2UuKnAuHQarTTsbMxhEfJ_DMmfllAHLAogXCSprgHQ9r7P0s_42XIbheW2zogpC7UD_wtRdktni06NUz66WcEuil9sXxf8p_NsBauT62biSaDpCvpuTv5-w4nDvXx5t5471DkoVBO_q3j4SuYVziyoiFA9aibzGPu53A88VDg0MJ8dmEmlX19m_8bgwZVvNXBRYpSP_t09sNaX8z1tSgQxZzhSQQTVhzZLnX6-OJhpCtHWVaEEKzxdvaLIBXyv3lIqU2p0DSHfhRuyV51yL87xotoLAah5eR-DwIzeG8R3u9HSMydkvqwCXYcoMJcbtyBZmC9PMRQa_RAWsueX60OdkqNl4HE0Jw2jUw5mTns9TsRMyVXPghBqydmtHA0owz8izelmsPGVOf7zGtxGQIPsRKOV9ZM8hCtI__fNKeylv6K0fpCLpZCv2z6GEhDD8KP4oH3tkQ7rn6XIk9dtQAPLvEYFk9URIEiDyXo-zCQwiLQqc8zBTXRJb8C-HLs8cng9ohadDhLB3ud5EYgvu0wAcqo93CutBLSqp-WuM68fVQ6UThevsRtxFsjT2Gb1-_5wQKfg0V8_0SppJ5oQ}}'
       )
+      myHeaders.append('User-Agent', 'Gorilla Pack (pamplonapaulo@gmail.com)')
+      myHeaders.append('Access-Control-Allow-Origin', '*')
 
       const raw = JSON.stringify({
         from: {
