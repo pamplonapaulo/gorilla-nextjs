421856f429e1f57804dd84d8c86b22e644b00ad9
pamplonapaulo <25485709+pamplonapaulo@users.noreply.github.com>
2022-05-30 21:47:03 -0300

    strapi api user me endpoint

--- src/components/PackPanel/packPanel.tsx
+++ src/components/PackPanel/packPanel.tsx
@@ -80,7 +80,8 @@ const PackPanel = ({ ...panelData }: PanelData) => {
       const fetchUser = async () => {
         try {
           const response = await axios.get<UserData>(
-            process.env.NEXT_PUBLIC_API_URL + '/users/' + session?.id,
+            process.env.NEXT_PUBLIC_API_URL + '/users/me',
+            // process.env.NEXT_PUBLIC_API_URL + '/users/' + session?.id,
             {
               headers,
             }
